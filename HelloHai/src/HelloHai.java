188c751682fae1cd818f3b721a615dadd5d4e5da
Hai Phan <htp200006@gmail.com>
2017-05-14 09:44:26 -0500

    Just starting a git repo for IntelliJ

---
+++ HelloHai/src/HelloHai.java
@@ -0,0 +1,9 @@
+/**
+ * Created by hp197 on 5/14/2017.
+ */
+public class HelloHai {
+    public static void main(String[] args){
+        System.out.print("Hello Hai");
+
+    }
+}
---
+++ HelloHai/src/Variables.java
@@ -0,0 +1,19 @@
+/**
+ * Created by hp197 on 5/14/2017.
+ */
+public class Variables {
+    public static void main(String[] args){
+        int myNumber = 5;
+        int myNumber2 = 12;
+        int myNumber3 = 6;
+
+        int myTotal = myNumber + myNumber2 + myNumber3;
+
+        myNumber = (10 + 5) + ( 2* 10);
+
+        myTotal = myNumber + myNumber2 + myNumber3;
+        System.out.println(myNumber);
+
+        System.out.printf("This is the total of 3 numbers: %d", myTotal);
+    }
+}
